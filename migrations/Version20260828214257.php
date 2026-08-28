ea6b6659a39cd27ba898031e4fdb12e032da0d13
Lydie Douti <lydiedouti09@gmail.com>
2026-08-28 11:33:38 -1200

    correction

---
+++ migrations/Version20260828214257.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260828214257 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE solution ADD description_complete_fr LONGTEXT DEFAULT NULL, ADD description_complete_en LONGTEXT DEFAULT NULL, ADD description_complete_de LONGTEXT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE solution DROP description_complete_fr, DROP description_complete_en, DROP description_complete_de');
+    }
+}
